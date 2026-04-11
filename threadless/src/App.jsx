9adcdbb5c772779b4d45c733a80246632066c680
Abhishek Anil <abhishekanil1729@email.com>
2026-04-11 22:41:16 +0530

    add footer component

--- threadless/src/App.jsx
+++ threadless/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import Footer from './components/footer/Footer'
 
 function App() {
 
   return (
     <>
+      <Footer />
     </>
   )
 }

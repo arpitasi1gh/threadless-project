f8c8b01f05bcb5fa79e673bd00190b2b3f0b9eba
Abhishek Anil <abhishekanil1729@email.com>
2026-04-15 15:29:28 +0530

    add signup page

--- threadless/src/App.jsx
+++ threadless/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Footer from './components/footer/Footer'
 import Home from './pages/home/home'
+import Signup from './components/signup/Signup'
 
 function App() {
   return (
     <>
       <Home />
+      <Signup />
       <Footer />
     </>
   )

4f5111598a2cb6495ead82746eabb47bbfd056b2
Manvir Singh Saran <manvirsaran3654@gmail.com>
2026-04-15 09:29:01 +0530

    Update navbar dropdowns

--- threadless/src/App.jsx
+++ threadless/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import React from 'react'
 import './App.css'
+import Header from './components/Header/Header'
 import Footer from './components/footer/Footer'
 
 function App() {
 
   return (
     <>
+      <Header />
       <Footer />
     </>
   )

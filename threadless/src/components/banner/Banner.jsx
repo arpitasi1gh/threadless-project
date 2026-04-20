46a142cf050a04f15ff7f5fd79bdce1cce214313
arpitasi1gh <arpitasi1gh@gmail.com>
2026-04-20 12:06:36 +0530

    added link in banner

--- threadless/src/components/banner/Banner.jsx
+++ threadless/src/components/banner/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import bannerImg from './banner-on-every-page.jpg'
 import bannerSmallImg from './banner-on-every-page-small.jpg'
 import './Banner.css'
@@ -6,10 +7,12 @@ import './Banner.css'
 export default function Banner() {
   return (
     <div className="banner">
-      <picture>
-        <source media="(max-width: 768px)" srcSet={bannerSmallImg} />
-        <img src={bannerImg} alt="Bloom or Bust - Tees from $15" />
-      </picture>
+      <Link to="/t-shirts">
+        <picture>
+          <source media="(max-width: 768px)" srcSet={bannerSmallImg} />
+          <img src={bannerImg} alt="Bloom or Bust - Tees from $15" />
+        </picture>
+      </Link>
     </div>
   )
 }
